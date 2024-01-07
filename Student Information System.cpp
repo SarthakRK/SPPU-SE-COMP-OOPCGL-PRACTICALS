a3e8db02f890a0e9f1f239e9d5fc3604aa8734d7
Sarthak Katare <kataresarthak001@gmail.com>
2024-01-07 21:16:58 +0530

    Update Student Information System.cpp

--- Student Information System.cpp
+++ Student Information System.cpp
@@ -4,8 +4,8 @@
 information system containing the following information: Name, Division,
 Date of Birth, Blood group, Contact address, Telephone number, Driving license no. and other.
 Construct the database with suitable member functions. Make use of constructor, default
-constructor, copy constructor, destructor, static member functions, friend class, this pointer, inline
-code and dynamic memory allocation operators-new and delete as well as exception handling.*/
+constructor, copy constructor, destructor, static member functions, friend class, this pointer,
+inline code and dynamic memory allocation operators-new and delete as well as exception handling.*/
 
 //Code:-
 
