ffb89ced30f8653d8d3b8e23cf255b403b753111
Sarthak Katare <kataresarthak001@gmail.com>
2024-01-07 21:16:28 +0530

    Update Publication Book Company Inheritance.cpp

--- Publication Book Company Inheritance.cpp
+++ Publication Book Company Inheritance.cpp
@@ -4,8 +4,7 @@
 Create a class publication that stores the title (a string) and price (type float) of publications.
 From this class derive two classes: book which adds a page count (type int) and tape which adds a playing time in minutes (type float).
 Write a program that instantiates the book and tape class, allows user to enter data and displays the data members.
-If an exception is caught, replace all the
-data member values with zero values.*/
+If an exception is caught, replace all the data member values with zero values.*/
 
 //Code:-
 
